61c2d0741188f7523c1e6065c67f2fcea38c6533
fish <2439765925@qq.com>
2023-07-31 10:14:48 +0800

    纯净的项目

---
+++ fish-common/src/main/java/com/fish/fishcommon/FishCommonApplication.java
@@ -0,0 +1,13 @@
+package com.fish.fishcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishCommonApplication.class, args);
+    }
+
+}
---
+++ fish-common/src/test/java/com/fish/fishcommon/FishCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fish.fishcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
