d06bd10e0d3c02e19a76e821e57e0b3b5a76f6a2
fish <2439765925@qq.com>
2023-07-31 11:03:34 +0800

    fish-client-sdk完善

--- fish-client-sdk/src/main/java/com/fish/fishclientsdk/FishClientSdkApplication.java
+++ fish-client-sdk/src/main/java/com/fish/fishclientsdk/FishClientSdkApplication.java
@@ -1,11 +1,21 @@
 package com.fish.fishclientsdk;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("fish.client")
+@Data
+@ComponentScan
 public class FishClientSdkApplication {
 
+    private String accessKey;
+
+    private String secretKey;
     public static void main(String[] args) {
         SpringApplication.run(FishClientSdkApplication.class, args);
     }
---
+++ fish-client-sdk/src/main/java/com/fish/fishclientsdk/model/User.java
@@ -0,0 +1,11 @@
+package com.fish.fishclientsdk.model;
+
+import lombok.Data;
+
+/**
+ * 用户
+ */
+@Data
+public class User {
+    private String username;
+}
