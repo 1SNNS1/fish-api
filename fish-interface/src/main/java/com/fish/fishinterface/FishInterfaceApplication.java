6bf3c242b073e00b98d8dbd4a5ce7c7786f53d74
fish <2439765925@qq.com>
2023-08-02 10:08:30 +0800

    fish-api完善-2

---
+++ fish-interface/src/main/java/com/fish/fishinterface/FishInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.fish.fishinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishInterfaceApplication.class, args);
+    }
+
+}
---
+++ fish-interface/src/test/java/com/fish/fishinterface/FishInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fish.fishinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
